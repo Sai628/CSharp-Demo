a88a49be4cc0cbe23bf7991e73a37892259206ff
Sai628 <sai100628@gmail.com>
2016-03-11 16:38:31 +0800

    add "attribute" demo

---
+++ CSharpDemo/TestAttribute.cs
@@ -0,0 +1,108 @@
+using System;
+
+namespace CSharpDemo
+{
+    [AttributeUsage(AttributeTargets.Class |
+    AttributeTargets.Constructor |
+    AttributeTargets.Field |
+    AttributeTargets.Method |
+    AttributeTargets.Property,
+    Inherited = false, AllowMultiple = true)]
+    public class DeBugInfo : Attribute
+    {
+        private int bugNo;
+        private string developer;
+        private string lastReview;
+        private string message;
+        
+        
+        public DeBugInfo(int no, string dev, string last)
+        {
+            bugNo = no;
+            developer = dev;
+            lastReview = last;
+        }
+        
+        
+        public int BugNo
+        {
+            get 
+            {
+                return bugNo;
+            }
+        }
+        
+        
+        public string Developer
+        {
+            get
+            {
+                return developer;
+            }
+        }
+        
+        
+        public string LastReview
+        {
+            get
+            {
+                return lastReview;
+            }
+        }
+        
+        
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+            set
+            {
+                message = value;
+            }
+        }
+    }
+    
+    
+    [DeBugInfo(45, "Zara Ali", "09/02/2016", Message = "Return type mismatch")]
+    [DeBugInfo(49, "Nuha Ali", "10/02/2016", Message = "Unused Variable")]
+    public class Rectangle3
+    {
+        protected double length;
+        protected double width;
+        
+        
+        public Rectangle3(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+        
+        
+        [DeBugInfo(55, "Zara Ali", "14/02/2016", Message = "Return type mismatch")]
+        public double GetArea()
+        {
+            return length * width;
+        }
+        
+        
+        [DeBugInfo(56, "Zara Ali", "16/02/2016")]
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    } 
+    
+    
+    public class TestAttribute
+    {
+        public static void DoTest()
+        {
+            Rectangle3 rect = new Rectangle3(13, 14);
+            rect.Display();
+        }
+    }
+}
--- CSharpDemo/TestMain.cs
+++ CSharpDemo/TestMain.cs
@@ -17,6 +17,7 @@ public class TestMain
         TestPreprocessor.DoTest();
         TestException.DoTest();
         TestFileIO.DoTest();
+        TestAttribute.DoTest();
     }
 }
 
